556522ba027e8f6511d1185ceec0c2c392b6531f
Phan101 <fk_tp@live.no>
2021-11-18 14:23:16 +0100

    Rettet skrivefei

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 server.set("port", PORT);
 
-const blogposts = rquire("./modules/blogposts.js");
+const blogposts = require("./modules/blogposts.js");
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
 
 server.use(blogposts);
 
-server.use(function(err, rq, res, next){
+server.use(function(err, req, res, next){
 	res.status(500).json({
 		error: "Something went wrong on the server!",
 		descr: err
